054e9583562c2a84065d9f9ba3673d8c07da51ce
ayiyaha-hyd <ayiyaha@yeah.net>
2021-04-09 15:32:57 +0800

    创建空工程

---
+++ spring-boot-shiro/src/test/java/com/hyd/SpringBootShiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootShiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
